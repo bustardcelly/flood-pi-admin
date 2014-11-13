9fe4a64115690da4b97f8fd454f1f0132307bf2b
Todd Anderson <bustardcelly@gmail.com>
2014-11-13 16:29:07 -0500

    proper traversl of list from dom module.

--- client/script/floodpi-time-formatter.js
+++ client/script/floodpi-time-formatter.js
@@ -1,6 +1,6 @@
 var dom = require('dom');
 var items = dom('.last-reading-date');
 
-dom.forEach(function(item) {
+items.forEach(function(item) {
   item.innerHTML = new Date(item.innerHTML + ' UTC').toString();
 });
