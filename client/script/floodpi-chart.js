1153d2230e8a55acdc006299bb39ae0b13985784
Todd Anderson <bustardcelly@gmail.com>
2014-11-12 22:32:02 -0500

    debounce draw of chart to support responsive layout.

--- client/script/floodpi-chart.js
+++ client/script/floodpi-chart.js
@@ -1,4 +1,4 @@
-/*global Flotr*/
+/*global window, Flotr*/
 var dom = require('dom');
 var xhr = require("json-xhr");
 var events = require('component-event');
@@ -7,6 +7,18 @@ var host = '@serviceHost';
 var port = '@servicePort';
 var select = dom('#range-select')[0];
 var container = dom('#time-chart')[0];
+
+var debounce = function(delay, fn) {
+  var timeout;
+  return function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      fn.apply(null, args);
+    }, delay);
+  };
+};
+
 var data = JSON.parse(decodeURIComponent(container.dataset.reading));
 var timeline = function(d) {
   return d.map(function(item) {
@@ -28,14 +40,15 @@ var options = {
   HtmlText : false,
   title : 'Time'
 };
-var inflateChart = function(d) {
+var inflateChart = debounce(300, function(d) {
   var o = Flotr._.extend(Flotr._.clone(options), {});
+  data = d;
   return Flotr.draw(
     container,
     [d],
     o
   );
-};
+});
 
 var getNewRange = function(value) {
   var url = 'http://' + host + ':' + port + '/level?range=' + value;
@@ -52,4 +65,9 @@ events.bind(select, 'change', function(event) {
   event.preventDefault();
   var selection = this.value;
   getNewRange(selection);
+});
+
+events.bind(window, 'resize', function(event) {
+  event.preventDefault();
+  inflateChart(data);
 });
